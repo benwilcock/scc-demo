2c7bf6b7276b1fb903d6df7cdb7185290d6e0fa2
Ben Wilcock <bwilcock@pivotal.io>
2018-06-20 21:42:57 +0200

    fixed the test description

--- src/test/resources/contracts/helloService/shouldReturnBonjourBenRequestPathIsBen.groovy
+++ src/test/resources/contracts/helloService/shouldReturnBonjourBenRequestPathIsBen.groovy
@@ -2,7 +2,7 @@ package contracts.helloservice
 
 import org.springframework.cloud.contract.spec.Contract
 Contract.make {
-    description "should return Hello Ben when given name is Ben"
+    description "should return Bonjour Ben when given request path name is Ben"
     request{
         method GET()
         url("/bonjour/Ben") {
